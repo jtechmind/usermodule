326f90f0d207ab7a135a8143b6f92f0e91790cc9
jtechmind <jtechmind@gmail.com>
2024-11-18 15:25:02 +0530

    Initialize User Module Package Structure.

---
+++ usermodule/usermodule/src/test/java/com/jtech/usermodule/UsermoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtech.usermodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
