846e47204e60cf87569618250082adbd9f449f97
jtechmind <jtechmind@gmail.com>
2024-11-21 02:02:30 +0530

    Add @Entity and @Data annotations.

--- usermodule/usermodule/src/main/java/com/jtech/usermodule/model/User.java
+++ usermodule/usermodule/src/main/java/com/jtech/usermodule/model/User.java
@@ -1,13 +1,31 @@
 package com.jtech.usermodule.model;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+@Data
+@Entity
+@Table(name = "usermodule")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(nullable = false,unique = true)
     private String userName;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false,unique = true)
     private String email;
+
+    @Enumerated(EnumType.STRING)
     private Role role = Role.USER;
+
     private LocalDateTime createdAt = LocalDateTime.now();
 
 }
