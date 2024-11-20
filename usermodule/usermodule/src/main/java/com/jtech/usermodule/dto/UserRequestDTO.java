dfbcab9658928c079ba85a4830c5b18c7411b36e
jtechmind <jtechmind@gmail.com>
2024-11-21 01:34:26 +0530

    Add UserRequestDTO for user input.

---
+++ usermodule/usermodule/src/main/java/com/jtech/usermodule/dto/UserRequestDTO.java
@@ -0,0 +1,9 @@
+package com.jtech.usermodule.dto;
+
+import lombok.Data;
+@Data
+public class UserRequestDTO {
+    private String userName;
+    private String email;
+    private String password;
+}
